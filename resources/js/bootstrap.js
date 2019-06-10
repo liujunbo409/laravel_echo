c6e2091f65367c83d318ee2f3431c9b86340bd09
liujunbo409 <514026959@qq.com>
2019-06-10 18:43:29 +0800

    安全性测试

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -34,6 +34,20 @@ let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+import Echo from 'laravel-echo'
+
+    window.io = require('socket.io-client');
+
+    window.Echo = new Echo({
+        broadcaster: 'socket.io',
+        // key: '922db1f6a0cb873bb6442e89a29f1dba'
+        // key: process.env.MIX_PUSHER_APP_KEY,
+        // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+        // encrypted: true
+        host:window.location.hostname+':6001',
+        // csrfToken: token.content
+    });
+
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
@@ -44,17 +58,4 @@ if (token) {
  * allows your team to easily build robust real-time web applications.
  */
 
-import Echo from 'laravel-echo'
-
-window.io = require('socket.io-client');
-
-window.Echo = new Echo({
-    broadcaster: 'socket.io',
-    // key: '922db1f6a0cb873bb6442e89a29f1dba'
-    // key: process.env.MIX_PUSHER_APP_KEY,
-    // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    // encrypted: true
-    host:window.location.hostname+':6001',
-    // csrfToken: token.content
-});
 
